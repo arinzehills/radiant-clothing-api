3373ff6cde23a3557543b30ada04fae5cc309806
hamza <hamzah@paritie.com>
2022-12-25 06:36:42 +0100

    worked on the payment section

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const { default: helmet } = require("helmet");
 const app = express();
-const port = 3000 || process.env.PORT;
+const shortId = require("shortid");
+const Razorpay = require("razorpay");
+const paymentRouter = require("./routes/payment");
+
+const port = 3002 || process.env.PORT;
 const http = require("http");
 const server = http.createServer(app);
 const cors = require("cors");
 
-// const db = process.env.DB_URL;
-const db = "mongodb://localhost:27017/radiant_db";
+const db = process.env.DB_URL;
+// const db = "mongodb://localhost:27017/radiant_db";
 require("./config/mongo.js")(db);
 app.use(express.json()); //making sure the server can use json, this is use to make the app able to use json
 app.use(helmet());
@@ -18,11 +22,14 @@ app.get("/", function (req, res) {
   res.setHeader("Content-Type", "text/html");
   res.end("<h1>Radiant Clothin API</h1>");
 });
+
 // routes
 app.use("/", require("./routes/user.route"));
 app.use("/admin", require("./routes/adminroutes/addcategory.route"));
 app.use("/admin", require("./routes/adminroutes/products.route"));
+app.use("/payment", paymentRouter);
 // app.use("/admin", require("./routes/adminroutes/addcategory.route"));
+
 
 server.listen(port, () => {
   console.log(`Listening on port:: http://localhost:${port}`);
